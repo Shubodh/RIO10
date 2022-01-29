45abb74888a9b0cae90e3d867dc8014fc6f314e0
shubodhsai <p.saishubodh@gmail.com>
2022-01-29 06:50:39 +0530

    setup.sh has issues, fixed it. Now can run setup.sh to do the setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # Install python dependencies
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 # download data
 if [[ ! -d "data/errors" ]]; then
@@ -13,14 +13,16 @@ if [[ ! -d "data/predictions" ]]; then
 	if [[ ! -f "data/predictions/predictions.zip" ]]; then
 		wget "http://campar.in.tum.de/files/RIO10/predictions.zip" -P data/predictions
 	fi
-	tar -xvf "data/predictions/predictions.zip" -C data/predictions
-	rm data/predictions/predictions.zip
+	#tar -xvf "data/predictions/predictions.zip" -C data/predictions
+	unzip data/predictions/predictions.zip -d data/predictions
+	#rm data/predictions/predictions.zip
 fi
 
 if [[ ! -f "data/seq10" ]]; then
 	wget "http://campar.in.tum.de/files/RIO10/rio10_val.zip" -P "data"
-	tar -xvf "data/rio10_val.zip" -C "data"
-	rm data/rio10_val.zip
+	#tar -xvf "data/rio10_val.zip" -C "data"
+	unzip data/rio10_val.zip -d data
+	#rm data/rio10_val.zip
 fi
 
 echo "Sucessfully setup."
